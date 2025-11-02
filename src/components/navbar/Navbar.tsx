a4d94f73995b58681c6267a8de92b62feeaf069c
Joe Chriszel Silva <90300759+JoeChriszelSilva@users.noreply.github.com>
2025-11-02 07:31:13 -0300

    add o footer e o navbar

---
+++ src/components/navbar/Navbar.tsx
@@ -0,0 +1,23 @@
+import { Link } from "react-router-dom";
+
+function Navbar() {
+  return (
+    <>
+      <div
+        className=" w-full flex justify-center p-4
+    bg-indigo-900 text-white"
+      >
+        <div className="container flex justify-between text-lg mx-8">
+          <Link to="/home" className="text-2xl font-bold">
+            Blog Pessoal
+          </Link>
+          <div className=" flex gap-4">
+            Postagem Temas Cadastrar Tema Perfil Sair
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Navbar;
